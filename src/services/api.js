ca1e5b5783ce454711a8743c6bfdf423c6241427
yatri vyas <you@example.com>
2026-05-12 00:58:24 +0530

    update

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
   // baseURL: "http://localhost:3000"
-  baseURL = "https://blog-project-backend-7jm6.onrender.com"
+  baseURL : "https://blog-project-backend-7jm6.onrender.com"
 });
 
 export const getUsers = () => API.get("/users");
